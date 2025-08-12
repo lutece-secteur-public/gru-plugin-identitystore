fd525943b6de6517033e8020b2dc88f485ac502d
sebastien leridon <sebleridon@laposte.net>
2025-08-12 17:04:06 +0200

    LUT-24948 : fix sql create script

--- src/sql/plugins/identitystore/plugin/create_db_identitystore.sql
+++ src/sql/plugins/identitystore/plugin/create_db_identitystore.sql
@@ -40,7 +40,7 @@ CREATE TABLE identitystore_identity
     PRIMARY KEY (id_identity)
 );
 
-CREATE INDEX identitystore_identity_connection_id ON identitystore_identity (connection_id);
+CREATE INDEX identitystore_identity_connection_id ON identitystore_identity (lower(connection_id::text));
 CREATE INDEX identitystore_identity_customer_id ON identitystore_identity (customer_id);
 CREATE INDEX identitystore_identity_master_id ON identitystore_identity (id_master_identity);
 CREATE INDEX identitystore_identity_expiration_date ON identitystore_identity (expiration_date);
